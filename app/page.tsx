cdf095e26caed8a1d7f80cf571670e5ba2fbe9d2
ImTheWill <william.cerritos.133@my.csun.edu>
2025-10-23 13:11:58 -0700

    Project Setup
    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const page = () => {
+  return (
+    <div className=" flex  justify-center items-center h-screen">
+      <Button className="">Click</Button>
+    </div>
+  )
+}
+
+export default page
